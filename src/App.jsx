1ee4403b911542aec744ce310a1cb28da1bd795a
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-24 17:38:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ClassComponent from './components/ClassComponent'
+import './App.css'
+import FunComponent from './components/FuncComponent'
+
+function App() {
+
+  return (
+
+    <div>
+      <ClassComponent />
+      <FunComponent />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FuncComponent.jsx
@@ -0,0 +1,12 @@
+
+
+function FunComponent() {
+  return <div>Componente Funcional
+    <p>
+      <button>-</button>0
+      <button>+</button>
+    </p>
+  </div>
+}
+
+export default FunComponent
