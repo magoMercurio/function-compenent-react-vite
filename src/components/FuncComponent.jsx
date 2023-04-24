0a08c026939ec73760b848d862bc7932ec1a8efc
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-24 17:49:23 -0500

    Update FuncComponent.jsx

--- src/components/FuncComponent.jsx
+++ src/components/FuncComponent.jsx
@@ -1,10 +1,12 @@
-
+import { useState } from "react"
 
 function FunComponent() {
+  const [value, setValue] = useState(100)
+
   return <div>Componente Funcional
     <p>
-      <button>-</button>0
-      <button>+</button>
+      <button onClick={ () => setValue(value - 1) }>-</button>{ value }
+      <button onClick={ () => setValue(value + 1) }>+</button>
     </p>
   </div>
 }
