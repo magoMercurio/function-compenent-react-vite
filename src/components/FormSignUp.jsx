82d81d442d93e840f34f960489935d8b3ffc4672
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-24 18:44:44 -0500

    Update FormSignUp.jsx

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,15 +1,29 @@
-import * as React from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function FormsignUp() {
   return <form>
-    <label>Nombre</label>
-    <input type="text" name="" id="" />
-    <label>Apellido</label>
-    <input type="text" name="" id="" />    
-    <label>Correo electronico</label>
-    <input type="email" name="" id="" />
-    <label>Promociones</label>
+    <TextField
+      id='name'
+      label='Nombre'
+      variant='outlined'
+      fullWidth
+      margin='normal'
+    />
+    <TextField
+      id='LastName'
+      label='Apellidos'
+      variant='outlined' 
+      fullWidth
+      margin='normal'
+    />
+    <TextField
+      id='email'
+      label='Email'
+      variant='outlined'
+      fullWidth
+      margin='normal'
+    />
     <input type="checkbox" name="" id="" />
     <label>Novedades</label>
     <input type="checkbox" name="" id="" />
