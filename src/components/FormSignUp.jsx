af45c4c3207177ec4776701e5b924f6ab0e742ab
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-24 18:30:07 -0500

    add button

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -1,9 +1,19 @@
-
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function FormsignUp() {
   return <form>
+    <label>Nombre</label>
     <input type="text" name="" id="" />
-    <button>Resgistrarse</button>
+    <label>Apellido</label>
+    <input type="text" name="" id="" />    
+    <label>Correo electronico</label>
+    <input type="email" name="" id="" />
+    <label>Promociones</label>
+    <input type="checkbox" name="" id="" />
+    <label>Novedades</label>
+    <input type="checkbox" name="" id="" />
+    <Button variant='contained' >Resgistrarse</Button>
   </form>
 }
 
