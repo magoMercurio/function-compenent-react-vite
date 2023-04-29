bfcda12db11c1cefbffbe91cc4650ff503938148
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-29 18:28:58 -0500

    Update FormSignUp.jsx
    validacion de los inputh nombre apellido y email

--- src/components/FormSignUp.jsx
+++ src/components/FormSignUp.jsx
@@ -11,7 +11,84 @@ function FormsignUp({ handleSubmit }) {
   const [email, setEmail] = useState('')
   const [prom, setProm] = useState(true)
   const [nov, setNov] = useState(false)
+
+  const [errorsName, setErrorsName] = useState ({
+    name: {
+      error: false,
+      message: 'Deben ser al menos 3 caracteres'
+    }
+  })
+
+  const [errorsLastName, setErrorsLastName] = useState({
+    lastName: {
+      error: false,
+      message: 'Deben ser al menos 3 caracteres'
+    }
+  })
+
+  const [errorsEmail, setErrorsEmail] = useState({
+    email: {
+      error: false,
+      message: 'Deben ser al menos 6 caracteres'
+    }
+  })
   
+  //VALIDADORES DE NOMBRE Y APELLIDOS
+  function validarNombre(nombre) {
+    if (nombre.length >= 3) {
+      return { 
+        name: {
+          error: false, 
+          message: '' 
+        } 
+      }
+    } else {
+      return { 
+        name:{ 
+          error: true,
+          message: 'Deben ser al menos 3 caracteres' 
+        }
+      }
+    }
+  }
+
+  function validarApellido(apellido) {
+    if (apellido.length >= 3) {
+      return { 
+        lastName: {
+          error: false, 
+          message: '' 
+        } 
+      }
+    } else {
+      return { 
+        lastName:{ 
+          error: true,
+          message: 'Deben ser al menos 3 caracteres' 
+        }
+      }
+    }
+  }
+
+    //VALIDADOR DE EMAIL
+    function validarEmail(email) {
+      if (email.length >= 6) {
+        return {
+          email: {
+            error: false,
+            message: ''
+          }
+        }
+      } else {
+        return {
+          email: {
+            error: true,
+            message: 'Deben ser al menos 6 caracteres'
+          }
+        }
+      }
+    }
+
   return <form onSubmit={ (e) => {
     e.preventDefault()
     handleSubmit({name, lastName, email, prom, nov})
@@ -27,6 +104,11 @@ function FormsignUp({ handleSubmit }) {
         setName(e.target.value)
       }}
       value={ name }
+      error={ errorsName.name.error }
+      helperText={ errorsName.name.error ? errorsName.name.message : ''}
+      onBlur={ (e) => {
+        setErrorsName(validarNombre(e.target.value))
+      }}
     />
     <TextField
       id='lastName'
@@ -38,6 +120,11 @@ function FormsignUp({ handleSubmit }) {
         setLastName(e.target.value)
       }}
       value={ lastName }
+      error={ errorsLastName.lastName.error }
+      helperText={ errorsLastName.lastName.error ? errorsLastName.lastName.message : ''}
+      onBlur={ (e) => {
+        setErrorsLastName(validarApellido(e.target.value))
+      }}
     />
     <TextField
       id='email'
@@ -49,6 +136,11 @@ function FormsignUp({ handleSubmit }) {
         setEmail(e.target.value)
       }}
       value={ email }
+      error={ errorsEmail.email.error }
+      helperText={ errorsEmail.email.error ? errorsEmail.email.message : ''}
+      onBlur={ (e) => {
+        setErrorsEmail(validarEmail(e.target.value))
+      }}
     />
     <FormGroup>
       <FormControlLabel 
